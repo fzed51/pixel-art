f3e04b228690c1308c6f02dc859a001200dc3c24
fzed51 <fzed51+git@gmail.com>
2017-03-15 00:16:10 +0100

    Initialisation du routeur

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
 var route = new Route();
+
+Array.from(document.querySelector('nav a')).forEach((item) => {
+    item.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        route.go(ev.target.href);
+    })
+});
--- public/js/route.js
+++ public/js/route.js
@@ -1,13 +1,29 @@
 class Route {
-    constructor(){
-        this._routes = [];
-        Array.from(document.querySelectorAll('div.page')).forEach((item)=>{
-            this._routes.push(
-                new Page (
-                    item.id, 
-                    new RegExp(item.dataset.route)
-                    )
-                );
+
+    constructor() {
+        this._routes = new Map();
+        Array.from(document.querySelectorAll('div.page')).forEach((item) => {
+            if (!item.dataset.route)
+                return;
+            this._createRoute(new RegExp(item.dataset.route), item.id)
         });
+        this._InitEvent();
     }
+
+    _createRoute(route, id) {
+        if (this._routes.has(route))
+            return console.warn(`La page ${route} existe déjà !`);
+        this._routes.set(route, new Page(id, route));
+    }
+
+    _InitEvent() {
+        const _onchange = this._onchange.bind(this);
+        document.addEventListener('DOMContentLoaded', _onchange);
+        window.addEventListener('popstate', _onchange);
+    }
+
+    _onchange() {
+        console.log('onChange');
+    }
+
 }
